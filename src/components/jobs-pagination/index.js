97693e1413d363a10223cfb90a8897e47f23c788
metrnametr <patronius1515@gamil.com>
2019-06-10 00:14:33 +0300

    add errorboundry

--- src/components/jobs-pagination/index.js
+++ src/components/jobs-pagination/index.js
@@ -5,30 +5,44 @@ import './style.css'
 class JobsPagination extends Component {
     
     state = {
-        countPage: []
+        countPage: [],
+        visiblePage: []
     }
     itemsPagination = () => {
-        const { pageCount } = this.props;
+        const { pageCount, currentPage } = this.props;
         const newArray = []
         for(let i = 1; i <= pageCount; i++){
             newArray.push(i)
         }
+
         this.setState({ countPage: [...this.state.countPage, ...newArray]})
+
+
+        const sliceStart = (((parseInt(currentPage) - 3) > 0)  ? (parseInt(currentPage) - 3) : 0);
+        const sliceEnd = ( ((parseInt(currentPage) - 1) > 0) ? (parseInt(currentPage) + 3) : (parseInt(currentPage) + 5))
+        const sliceArray = newArray.slice( sliceStart , sliceEnd )
+        this.setState({ visiblePage: [...sliceArray]})
+        
     }
-    
+
     slide = (value) => {
         const { toggleCurrentPage, currentPage } = this.props
         toggleCurrentPage( parseInt(currentPage) + value)
     }
-
+    componentDidMount(){
+        this.itemsPagination()
+    }
     componentDidUpdate(prevProps){
         if(this.props.pageCount !== prevProps.pageCount){
+            this.itemsPagination()
+        }
+        if(this.props.currentPage !== prevProps.currentPage){
             this.itemsPagination()
         }
     }
     renderListPagination = () => {
         const { toggleCurrentPage, currentPage } = this.props
-        return this.state.countPage.map( (i, index) => 
+        return this.state.visiblePage.map( (i, index) => 
                     <li key={index} className={(parseInt(currentPage) === i)  ? 'page-item active' : 'page-item'}>
                         <Link 
                             className="page-link" 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './components/app'
 import API from './API/api'
 import { WithServicesProvider } from './components/with-context'
-
+import ErrorBoundry from './components/error-boundry'
 const api = new API()
 
 ReactDOM.render(
 <WithServicesProvider value={api}>
-    <App />
+    <ErrorBoundry>
+        <App />
+    </ErrorBoundry>
 </WithServicesProvider>, 
 document.getElementById('root'));
 
